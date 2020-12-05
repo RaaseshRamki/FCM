4cb014158c70eb0b0a3ff5d4462153c2a5239862
raaseshramakrishnan <raasesh.softborne@gmail.com>
2020-12-05 11:43:27 +0530

    FCM initialized and subscribing.

--- FCMExample/FCMLibrary/src/main/java/com/pushengage/fcmlibrary/FCMHelper.java
+++ FCMExample/FCMLibrary/src/main/java/com/pushengage/fcmlibrary/FCMHelper.java
@@ -1,8 +1,72 @@
 package com.pushengage.fcmlibrary;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class FCMHelper {
 
-    public static boolean getPushNotificationPermission() {
+    private static Context context;
+    private static String TAG = "FCMHelper";
+
+    public static boolean getPermissionStatus() {
         return true;
+    }
+
+    public static boolean subscribe() {
+        FirebaseApp.initializeApp(context);
+        FirebaseMessaging.getInstance().subscribeToTopic("PE")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Boolean msg = true;
+                        if (!task.isSuccessful()) {
+                            msg = false;
+                        }
+                        Log.d(TAG, String.valueOf(msg));
+                        Toast.makeText(context, String.valueOf(msg), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        return true;
+    }
+
+    public static boolean getSubscriptionStatus() {
+        return true;
+    }
+
+    public static String getSubscriptionToken() {
+        return "";
+    }
+
+
+    private FCMHelper(Context context, String message) {
+        // Initialize your Library here
+        this.context = context;
+    }
+
+    class Builder {
+        private Context context;
+        private String message;
+
+        public Builder addContext(Context context) {
+            this.context = context;
+            return this;
+        }
+
+        public Builder addMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public FCMHelper build() {
+            return new FCMHelper(context, message);
+        }
     }
 }
